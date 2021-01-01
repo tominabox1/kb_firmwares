b719a3606c9e9d16051ffa46d0f0d09796c9ca3d
TJ <tom.campie@gmail.com>
2021-01-01 09:02:57 -0600

    Update to latest rgb_matrix driver code

--- Firmware/leftover30_arm/rules.mk
+++ Firmware/leftover30_arm/rules.mk
@@ -17,11 +17,11 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGB_MATRIX_ENABLE = WS2812        # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812        # Enable WS2812 RGB underlight.
 WS2812_DRIVER = spi
 API_SYSEX_ENABLE = no
 TAPDANCE_ENABLE = yes
-LTO_ENABLE = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
